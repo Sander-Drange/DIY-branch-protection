43c2bc7a272a6ebbae35c43dd8b41f0707f04525
Sander Drange <sadr002@student.kristiania.no>
2024-10-29 11:09:03 +0100

    initial commit

---
+++ src/main/java/com/example/DIY_brnach_protection/DiyBrnachProtectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.DIY_brnach_protection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyBrnachProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyBrnachProtectionApplication.class, args);
+		System.out.println("Herro");
+	}
+
+}
---
+++ src/test/java/com/example/DIY_brnach_protection/DiyBrnachProtectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DIY_brnach_protection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyBrnachProtectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
